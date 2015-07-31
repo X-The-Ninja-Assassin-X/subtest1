606a46790c737a12bba5dfe7e3d0bccee2a1eee1
X-The-Ninja-Assassin-X <jtembo1993@gmail.com>
2015-07-30 22:29:46 -0400

    JOE
    this is the caffene program done, all up to the point before you went to
    bed on  thursday

---
+++ app/src/main/java/subtestmath.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Joseph on 7/30/2015.
+ */
+public class subtestmath {
+
+    public void main()
+    {
+        int x=1;
+        int y=5;
+       int z=y+x;
+        System.out.print(z);
+
+    }
+}
