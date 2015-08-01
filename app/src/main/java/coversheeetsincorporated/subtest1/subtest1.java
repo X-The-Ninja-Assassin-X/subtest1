9d511125e0c0816706a68a17742129a8336a2290
X-The-Ninja-Assassin-X <jtembo1993@gmail.com>
2015-07-31 20:13:34 -0400

    joe change 2
    adding the coding to the buttons and the math equations

--- app/src/main/java/coversheeetsincorporated/subtest1/subtest1.java
+++ app/src/main/java/coversheeetsincorporated/subtest1/subtest1.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class subtest1 extends ActionBarActivity {
@@ -16,12 +17,40 @@ public class subtest1 extends ActionBarActivity {
     Button detailed;
     LinearLayout simplelayout;
     LinearLayout detaliedlayout;
-
+    Button cardio_yes;
+    Button cardio_no;
+    Button smoke_yes;
+    Button smoke_no;
+    Button alcohol_yes;
+    Button alcohol_no;
+    Button diet_yes;
+    Button diet_no;
+    Button pregnant_yes;
+    Button pregnant_no;
+    Button grapefruit_yes;
+    Button grapefruit_no;
+    Button contra_yes;
+    Button contra_no;
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.subtestlay);
-
+            //h1/2=f*(log(caffene))/(log(2.875))
+        //f=weight*cardio*smoke*pregnant*alcohol*grapefruit*diet*contraceptives
+        double weight=1;
+        //weight= 1-(0.003434307937522*(weight-150)
+        double cardio=1;
+        double smoke=1;
+        double pregnant =1;
+        double alcohol =1;
+        double grapefruit =1;
+        double diet =1;
+        double contraceptives =1;
+        double h=0;
+        double f=1;
+        double a=1;
+        f=weight*cardio*smoke*pregnant*alcohol*grapefruit*diet*contraceptives;
+        h=f*Math.log(a)/Math.log(2.875);
 
             layout=(LinearLayout) findViewById(R.id.layout);
 
@@ -35,16 +64,30 @@ public class subtest1 extends ActionBarActivity {
                 }
             }
             );
-            detailed.setOnClickListener(new View.OnClickListener() {
+
+        cardio_yes.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClicck(View){
+                Toast.makeText()
+
+
+            detailed.setOnClickListener(new View.OnClickListener()
+
+            {
+                @Override
+                public void onClick (View v){
                 setContentView(R.layout.detailedlayout);
             }
-        });
 
-    }
 
-    @Override
+            }
+
+            );
+
+
+        }
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_subtest1, menu);
